47c0fac458fdf8336c19a2c6f72c26642ef5596e
Speedy <speedyunicorn777@gmail.com>
2015-09-30 21:00:45 -0700

    added files

---
+++ L3-Variables.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var swiftVersion = "Hello, Swift 2.0"
+
+var str = "Hello, playground"
+
+str
+
+str = "Hello"
+
+let MY_NAME = " Nikita"
+
---
+++ L4-Numbers.playground/Contents.swift
@@ -0,0 +1,4 @@
+//: Playground - noun: a place where people can play
+
+
+var a: Float = 10.9
