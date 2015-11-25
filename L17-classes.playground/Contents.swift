5eec2195e1c95b778aef21651f15002c14cd8b1e
Speedy <speedyunicorn777@gmail.com>
2015-11-24 22:30:40 -0800

    more lessons

--- L17-classes.playground/Contents.swift
+++ L17-classes.playground/Contents.swift
@@ -31,3 +31,59 @@ var p1 : animal = animal(nP: "Matt", pA: "poodle", pB: "parrot", fA: "dolphin")
 p1.fullprofile()
 
 
+class Person {
+    var firstName: String
+    var lastName: String
+    var yearOfBirth: Int
+    
+    init ( fN: String, lN: String, yOB: Int){
+        self.firstName = fN
+        self.lastName = lN
+        self.yearOfBirth = yOB
+    }
+    
+    var fullName: String {
+        return "\(firstName), \(lastName)"
+    }
+    
+    var age: Int {
+        let currentYear = NSCalendar.currentCalendar().component(NSCalendarUnit.Year, fromDate: NSDate())
+        return currentYear - yearOfBirth
+    }
+}
+
+
+var p2: Person = Person(fN:
+    "Alex", lN: "Black", yOB: 1990)
+
+p2.yearOfBirth
+p2.fullName
+p2.age
+
+
+let now = NSDate()
+let cal = NSCalendar.currentCalendar()
+let comp = cal.component(NSCalendarUnit.Year, fromDate: now)
+
+
+class Employee : Person {
+    var hourlySalary: Double = 9.47
+    
+    init(fN: String, lN: String, yOB: Int, hSal: Double) {
+        self.hourlySalary = hSal
+        super.init(fN: fN, lN: lN, yOB: yOB)
+    }
+    
+    func weeklySalary(numberOfHoursWorked: Double) -> Double {
+        return hourlySalary * numberOfHoursWorked
+    }
+}
+
+
+var e1: Employee = Employee(fN: "Mat", lN: "Black", yOB: 1750, hSal: 17.0)
+
+e1.fullName
+e1.weeklySalary(40)
+
+
+
