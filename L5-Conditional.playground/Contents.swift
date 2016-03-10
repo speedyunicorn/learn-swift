ead54f863a01ccec87d4bf94f07d1c8af97e0304
Speedy <speedyunicorn777@gmail.com>
2016-03-09 21:01:26 -0800

    few updates

--- L5-Conditional.playground/Contents.swift
+++ L5-Conditional.playground/Contents.swift
@@ -3,3 +3,69 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var answers = [
+    "I'm not a mindreader!",
+    "All indicators point to YES!",
+    "All indicators point to NO!",
+    "Watch your language!",
+    "It seems highly unlikely.",
+    "Please speak up!",
+    "It seems highly likely.",
+    "Yes, indeed!",
+    "Absolutely not!",
+    "Please ask again later...",
+    "Seriously?",
+    "That was a dumb question.",
+    "I was wondering the same thing!",
+    "I can't answer that truthfully.",
+    "What do you think?",
+    "OMG YES!",
+    "OMG NO!",
+    "Imagine that!",
+    "Really?",
+    "Did you just ask that?",
+    "Of course.",
+    "Ask a higher power.",
+    "The force is with you.",
+    "The force is not near.",
+    "You're unbelievable!",
+    "Can I get a translation please?",
+    "I'm gonna guess... NO!",
+    "I'm gonna guess... YES!",
+    "It would seem so.",
+    "That does not seem to be true.",
+    "Does a bear sh... never mind.",
+    "I need a new profession!",
+    "The odds are against that.",
+    "The odds are in your favor.",
+    "Can we skip this one?",
+    "Ask a different question please.",
+    "I'm doing good, yes I know you didn't ask.",
+    "My connection to the stars was lost.",
+    "Do you want the truth?",
+    "I can see no answer but yes.",
+    "gag",
+    "ghjghjgjhg",
+    "ttttttt"
+]
+
+answers[0]
+answers.count
+answers[39]
+
+answers[answers.count - 1]
+
+var numbers = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    9,
+    45
+]
+numbers.count
+numbers[numbers.count-1]
+
